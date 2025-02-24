0dfe0f3ff016abaa4d44ed8b22374a259e122027
kolesnikaleksv <kolesnikaleksv@gmail.com>
2025-02-24 14:04:51 +0200

    Add new section for cards and StartupCard component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import SearchForm from '@/components/SearchForm';
+import StartupCard from '@/components/StartupCard';
 
 export default async function Home({
   searchParams,
@@ -6,6 +7,19 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   console.log(await searchParams, 'hello');
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: 'Olexandr' },
+      _id: 1,
+      description: 'This is a description',
+      image:
+        'https://variety.com/wp-content/uploads/2024/11/The-Wild-Robot-HTW-Featured.jpg?w=1360&h=906&crop=1',
+      category: 'Robots',
+      title: 'The wild robot',
+    },
+  ];
   const query = (await searchParams).query;
   return (
     <>
@@ -18,6 +32,22 @@ export default async function Home({
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competition
         </p>
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search result for ${query}` : 'All Startups'}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
