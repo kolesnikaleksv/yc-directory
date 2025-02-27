af3750917145399fa7e78c671c34ef0caeb7c1cf
kolesnikaleksv <kolesnikaleksv@gmail.com>
2025-02-27 16:47:34 +0200

    Fetch data from sanity with the PPR strategy

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,18 +1,19 @@
-import { sanityFetch } from '@/sanity/lib/live';
+import { client } from '@/sanity/lib/client';
 import { STARTUP_BY_ID_QUERY } from '@/sanity/lib/queries';
+import { notFound } from 'next/navigation';
 import React from 'react';
+
+export const experimental_ppr = true;
 
 const page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
 
-  const { data: post } = await sanityFetch({
-    query: STARTUP_BY_ID_QUERY,
-    params: await params,
-  });
-  console.log(post, null, 2);
+  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+  if (!post) return notFound();
+  console.log(post);
   return (
     <>
-      <h1 className="text-2xl">This number is id of the page {id}</h1>
+      <h1 className="text-2xl">{post.title}</h1>
     </>
   );
 };
