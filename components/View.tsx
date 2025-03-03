aff5c0f197ec0e33a159a1e098519b8e6fd4c3f6
kolesnikaleksv <kolesnikaleksv@gmail.com>
2025-03-03 10:51:20 +0200

    Add write client componet that halp to update number of views

--- components/View.tsx
+++ components/View.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Ping from '@/components/Ping';
 import { STARTUP_VIEWS_QUERY } from '@/sanity/lib/queries';
 import { client } from '@/sanity/lib/client';
+import { writeClient } from '@/sanity/lib/write-client';
+import { after } from 'next/server';
 
 const View = async ({ id }: { id: string }) => {
   const { views: totalViews } = await client
@@ -9,10 +11,18 @@ const View = async ({ id }: { id: string }) => {
     .fetch(STARTUP_VIEWS_QUERY, { id });
 
   const formanNumber = (views: number): string => {
-    return `${views === 1 ? views + ' View' : views + 'Views '} `;
+    return `${views === 1 ? views + ' View' : views + ' Views'} `;
   };
 
-  // TODO: Update the number of views
+  // Update the number of views
+
+  after(
+    async () =>
+      await writeClient
+        .patch(id)
+        .set({ views: totalViews + 1 })
+        .commit()
+  );
 
   return (
     <div className="view-container">
