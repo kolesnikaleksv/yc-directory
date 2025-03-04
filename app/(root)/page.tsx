3ccb491d10f2604e22cfd526a29a338df336cabe
kolesnikaleksv <kolesnikaleksv@gmail.com>
2025-03-04 16:12:37 +0200

    Add authentification and find the id of user

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import { auth } from '@/auth';
 import SearchForm from '@/components/SearchForm';
 import StartupCard, { StartupCardType } from '@/components/StartupCard';
 import { sanityFetch, SanityLive } from '@/sanity/lib/live';
@@ -10,6 +11,8 @@ export default async function Home({
 }) {
   const query = (await searchParams).query;
   const params = { search: query || null };
+  const session = await auth();
+  console.log(session?.id);
   const { data: posts } = await sanityFetch({ query: STARTUP_QUERY, params });
 
   return (
